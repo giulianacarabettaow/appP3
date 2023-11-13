9d1f6677a1548f064c020aa0a46fdc6bd4f9e7ba
TimoHerrera <therrera@udesa.edu.ar>
2023-11-13 11:49:07 -0300

    palermitano

--- src/screens/Comments/Comments.js
+++ src/screens/Comments/Comments.js
@@ -8,7 +8,8 @@ class Comments extends Component {
     constructor(props){
         super(props)
         this.state={
-         
+        comentarios:[],
+        comment:'',
         }
     }
 
@@ -21,17 +22,91 @@ class Comments extends Component {
               comentarios:doc.data().comments.sort((a, b) => a.createdAt - b.createdAt).reverse() //esto es para que los comentarios aparezcan de manera ascendente
             }, () => console.log(this.state.data))
           })
+
+          db.collection("users").where("email", '==', auth.currentUser.email).onSnapshot((docs) => {
+            let userLoguedInfo = [];
+            docs.forEach((doc) => {
+              userLoguedInfo.push({
+                data: doc.data()
+              })
+            });
+            this.setState({
+              userLoguedInfo: userLoguedInfo,
+            });
+          });
+      }
+
+      comment(comentario) {
+        const commentAGuardar = {
+          ownerUsername: this.state.userLoguedInfo[0]?.data.userName,
+          ownerEmail: auth.currentUser.email,
+          createdAt: Date.now(),
+          description: comentario
+        }
+    
+        db.collection('posts').doc(this.props.route.params.id).update({
+          comments: firebase.firestore.FieldValue.arrayUnion(commentAGuardar)
+        })
+          .then(() => {
+            this.setState({
+              comment: '',
+              commentCount: this.state.commentCount + 1
+            });
+          })
+          
       }
     
-   
+      
     render(){
 
         return(
-          <Text>Comentarios</Text>
-
+            <View >
+            <View >
+              {this.state.commentCount == 0 ? 
+                  <Text>No comments yet...</Text>
+                : <>
+                  <Text >
+                    {this.state.commentCount} 
+                  </Text>
+                  <FlatList
+                        style={styles.flat}
+                        data={this.state.comentarios.sort((a, b) => b.createdAt - a.createdAt)}
+                        keyExtractor={( item ) => item.createdAt.toString()}
+                        renderItem={({item}) => 
+                                    <>
+                                        <View >
+                                            <TouchableOpacity  onPress={() => this.props.navigation.navigate('Go Back', {user: item.ownerEmail})}>
+                                                <Text >{item.ownerUsername}: </Text>
+                                            </TouchableOpacity> 
+                                            <Text >{item.description}</Text>
+                                        </View>
+                                    </>                  
+                                }/> 
+                </>
+              }
+              <View >
+                  <TextInput
+                   
+                    keyboardType='default'
+                    placeholder='Your comment!'
+                    onChangeText={ text => this.setState({comment:text}) }
+                    value={this.state.comment} />
+                  <TouchableOpacity onPress={() => this.comment(this.state.comment)}>
+                    <Text >
+                      Up Load
+                    </Text>
+                  </TouchableOpacity>
+              </View>
+    
+    
+            </View>
+          </View>
         )
-    }
+      }
+    
+
 }
+
 const styles = StyleSheet.create({
     formContainer:{
         paddingHorizontal:10,
